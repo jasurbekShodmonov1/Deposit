625962b62bb87bcb707f23bb360c2459c802b5b2
jasurbekShodmonov1 <jasurshodmonov21020@gmail.com>
2024-05-23 23:23:30 +0500

    Permission for deposit

--- deposit_project/settings.py
+++ deposit_project/settings.py
@@ -74,18 +74,23 @@ WSGI_APPLICATION = 'deposit_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'customers',
+#         'USER': 'postgres',
+#         'PASSWORD': 'jasur1803',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'customers',
-        'USER': 'postgres',
-        'PASSWORD': 'jasur1803',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
